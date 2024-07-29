5b9dc688ba0fd49ed0cec42e038a5668c702cdf9
Daniyal Zhanturin <daniyal5536@gmail.com>
2024-07-29 16:57:49 +0500

    feature: handle errors

--- internal/infrastructure/notify/notifier.go
+++ internal/infrastructure/notify/notifier.go
@@ -20,6 +20,9 @@ type Notifier struct {
 func (n Notifier) SendMessage(message string) error {
 	// Send message
 	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", n.BotToken)
+	if message == "" {
+		return fmt.Errorf("message is empty")
+	}
 	data := map[string]interface{}{
 		"text":       message,
 		"chat_id":    n.ChatID,
@@ -30,6 +33,10 @@ func (n Notifier) SendMessage(message string) error {
 	if err != nil {
 		log.Fatal(err)
 		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error sending message:", resp.Status)
+		return fmt.Errorf("error sending message: %s", resp.Status)
 	}
 	fmt.Println("Message sent", resp)
 	return nil
