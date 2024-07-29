8449163ac3fe85568372f6523817b33985812042
Daniyal Zhanturin <daniyal5536@gmail.com>
2024-07-29 18:58:19 +0500

    feature: move database file to the data folder

--- internal/infrastructure/database/connection.go
+++ internal/infrastructure/database/connection.go
@@ -1,13 +1,18 @@
 package database
 
 import (
+	"fmt"
 	"github.com/glebarez/sqlite"
 	"gorm.io/gorm"
+	"path/filepath"
 )
 
 func Connection(dsn string) *gorm.DB {
 	//db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
+	dsnWithFolder := filepath.Join("data", dsn)
+	fmt.Println("DSN:", dsnWithFolder)
+	db, err := gorm.Open(sqlite.Open(dsnWithFolder), &gorm.Config{})
+	//db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
